f4b51b80190edabcf29e4db9a638ab239fed6d16
Rodrigo Lueneberg <rlueneberg@gmail.com>
2022-10-26 20:06:09 -0300

    //Configuring Digital Ocean App Platform Adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
+//https://github.com/TorstenDittmann/svelte-adapter-static-digitalocean
+//import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import adapter from 'svelte-adapter-static-digitalocean';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,8 +10,20 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
+		//adapter: adapter()
+		adapter: adapter({
+            // default options are shown
+            pages: 'build',
+            assets: 'build',
+            fallback: '404.html',
+            precompress: false,
+            spec: '.do/spec.yaml',
+            name: ''
+        })
+
+
+	},
+
 };
 
 export default config;
