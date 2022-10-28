2953feb226a5189c9b343a1e9ddab455e8d56b32
Rodrigo Lueneberg <rlueneberg@gmail.com>
2022-10-28 12:20:01 -0300

    before implementing fetch and cache

--- src/routes/objectives/+page.svelte
+++ src/routes/objectives/+page.svelte
@@ -9,6 +9,7 @@
 <main>
     <h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">Objetivo de Estudo</h1>
     <ObjectiveForm/>
+    
     {#each $objectives as item}
         <Objective objective={item} index={item.id} />
     {/each}
