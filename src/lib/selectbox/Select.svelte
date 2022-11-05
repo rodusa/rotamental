92fe09cd6a58e34fcdedd3f6169ac638c299f999
Rodrigo Lueneberg <rlueneberg@gmail.com>
2022-11-05 12:40:22 -0300

    changes

--- src/lib/selectbox/Select.svelte
+++ src/lib/selectbox/Select.svelte
@@ -2,20 +2,21 @@
 	import Select from 'svelte-select';
 	import Item from './BeerItem.svelte';
 	import loadOptions from './fetch.js';
-	
+
+	let heading;
+	let svelteClass = '';
+
+
 	const items = ['Concurso Polícia Federal', 'Entrevista de Emprego', 'ENEM'];
 	
-	
-	const groupBy = (item) => item.group;
-	const optionIdentifier = 'id';
-  	const getOptionLabel = (option) => option.name;
-  	const getSelectionLabel = (option) => option.name;
+
 </script>
 
 
 <h2>Objetivo</h2>
 <div class="p-0 ml-0">
-<Select {items} inputStyles="padding-left: 0.5rem; "></Select>
+<Select {items} inputStyles="padding-left: 0.5rem;"></Select>
+
 </div>	
 <style>
 	/* 	CSS variables can be used to control theming.
